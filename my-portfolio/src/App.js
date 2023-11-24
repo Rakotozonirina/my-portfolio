10efe53d6e2a59a0fc856ca8aa6773c59a9cf105
Zo Nirina <tomzonirina@gmail.com>
2023-11-24 16:01:19 +0100

    feat: change of thing

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <>
-      
+      <h1 className='text-violet-400'>
+        Hello world!
+      </h1>
     </>
   );
 }
