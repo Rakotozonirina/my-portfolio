c2259187051dbdab13ad7c0038fa32125630b82d
Zo Nirina <tomzonirina@gmail.com>
2023-11-30 16:19:02 +0100

    feat: adding something

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Navbar from '../src/Components/Navbar'
 
 function App() {
   return (
-    <>
-      <h1 className='text-violet-400'>
-        Hello world!
-      </h1>
-    </>
+    <div className='w-full h-[100dvh] bg-gradient-to-b from-indigo-200 to-fuchsia-300'>
+      <Navbar/>
+    </div>
   );
 }
 
