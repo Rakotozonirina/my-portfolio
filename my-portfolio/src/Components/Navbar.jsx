e90a1545c387fc9a339bd506d312458fab878235
Zo Nirina <tomzonirina@gmail.com>
2023-11-30 19:27:42 +0100

    feat: finished of the responsive navbar

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Disclosure} from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: "Page d'acceuil", href: '#', current: false},
@@ -19,7 +19,10 @@ export default function Example() {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-16 items-center sm:max-md:grid sm:max-md:content-center sm:max-md:justify-center sm:max-md:relative max-sm:grid max-sm:content-center max-sm:justify-center justify-between">
+                <div>
+                    <h1 className='text-[#E0F4FF] font-semibold text-2xl' >Logo</h1>
+                </div> 
               <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -51,15 +54,10 @@ export default function Example() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+              <div className="absolute sm:max-md:absolute sm:max-md:right-0 inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <button className="w-[40px] cursor-pointer h-[28px] p-[2px] flex justify-between items-center border border-neutral-500 dark:bg-slate-900 bg-zinc-300 rounded-[30px] relative">
+                        <div className={`w-[15px] absolute h-[15px]  bg-indigo-950 dark:bg-white duration-200 rounded-full`}></div>
+                    </button>
               </div>
             </div>
           </div>
@@ -72,7 +70,7 @@ export default function Example() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-fuchsia-500',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
