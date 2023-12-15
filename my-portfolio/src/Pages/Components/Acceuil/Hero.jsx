4fe6edf81aa1ad880c977aca50012cdfaaaf0279
Zo Nirina <tomzonirina@gmail.com>
2023-12-15 08:38:26 +0100

    feat: finished of responsive

--- my-portfolio/src/Pages/Components/Acceuil/Hero.jsx
+++ my-portfolio/src/Pages/Components/Acceuil/Hero.jsx
@@ -6,9 +6,9 @@ import HeroZo from "../../../Assets/Acceuil/zo nirina.png"
 export function Hero(){
     return(
         <>
-            <img src={CircleOne} alt="circle" className="absolute md:max-lg:top-12 md:max-lg:left-0 top-12 right-60" />
-            <img src={HeroZo} alt="hero zo nirina" className="absolute bottom-0 right-64 block object-cover h-[70dvh]" />
-            <img src={IllustrationBottom} alt="illustration bottom" className="absolute bottom-0 right-36 block object-cover h-[80dvh]" />
+            <img src={CircleOne} alt="circle" className="absolute max-sm:left-4 sm:max-md:left-4 md:max-lg:top-12 md:max-lg:left-0 top-12 right-60" />
+            <img src={HeroZo} alt="hero zo nirina" className="absolute bottom-0 max-sm:right-0 right-64 block object-cover h-[70dvh]" />
+            <img src={IllustrationBottom} alt="illustration bottom" className="absolute bottom-0 max-sm:right-0 sm:max-md:right-0 right-36 block object-cover h-[80dvh]" />
         </>
     )
 }
