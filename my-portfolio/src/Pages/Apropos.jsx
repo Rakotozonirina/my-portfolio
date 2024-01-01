433d160af492f7f932a3e2729d58172a422e94a0
Zo Nirina <tomzonirina@gmail.com>
2024-01-01 14:28:02 +0100

    feat: struggle in the about page

--- my-portfolio/src/Pages/Apropos.jsx
+++ my-portfolio/src/Pages/Apropos.jsx
@@ -1,11 +1,32 @@
 import React from "react"
 import Navbar from "../Components/Navbar"
+import SvgJavascript from "./Components/Apropos/Svg/SvgJavascript"
+import CssSvg from "./Components/Apropos/Svg/CssSvg"
+
+
+function Illustration() {
+    return(
+        <>
+        <section className="flex">
+            <div className="border border-red-400">
+                    <SvgJavascript/>
+            </div>
+            <div className="border border-yellow-400">
+                    <CssSvg/>
+            </div>
+        </section>
+        </>
+    )
+}
 
 export function Apropos(){
     return(
         <>
+        <div className="h-[100dvh] w-full">
         <Navbar/>
         <h1>Sur moi</h1>
+        <Illustration/>
+        </div>
         </>
     )
 }
