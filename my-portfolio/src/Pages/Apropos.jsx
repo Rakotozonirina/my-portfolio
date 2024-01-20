d6462a86bf2f9918cb4ae381171e43ba08df6832
Zo Nirina <tomzonirina@gmail.com>
2024-01-20 08:23:38 +0100

    feat: adding the tailwind svg element

--- my-portfolio/src/Pages/Apropos.jsx
+++ my-portfolio/src/Pages/Apropos.jsx
@@ -1,26 +1,36 @@
 import React from "react"
 import Navbar from "../Components/Navbar"
-import MyJs from "./Components/Apropos/Svg/MyJs"
+//import MyJs from "./Components/Apropos/Svg/MyJs"
 import HtmlSvg from "./Components/Apropos/Svg/HtmlSvg"
-import CssSvg from "./Components/Apropos/Svg/CssSvg"
+//import CssSvg from "./Components/Apropos/Svg/CssSvg"
 import GitSvg from "./Components/Apropos/Svg/GitSvg"
+import GitHubSvg from "./Components/Apropos/Svg/GitHubSvg"
+import TailwindCssSvg from "./Components/Apropos/Svg/TailwindCssSvg"
 
 
 function Illustration() {
     return(
         <>
         <section className="flex flex-wrap">
+            {/*
             <div className="border border-green-400">
                     <CssSvg/>
             </div>
             <div className="border border-yellow-400">
                 <MyJs/>
             </div>
+            */}
             <div className="border border-amber-400">
                 <HtmlSvg/>
             </div>
             <div className="border border-amber-400">
                 <GitSvg/>
+            </div>
+            <div className="border border-amber-400">
+                <GitHubSvg/>
+            </div>
+            <div className="border border-amber-400">
+                <TailwindCssSvg/>
             </div>
         </section>
         </>
