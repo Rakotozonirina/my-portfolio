4183957bb671badd7fc1a59696329758327383b2
Zo Nirina <tomzonirina@gmail.com>
2024-02-09 23:36:05 +0100

    feat: adding text in contact

--- my-portfolio/src/Pages/Contact.jsx
+++ my-portfolio/src/Pages/Contact.jsx
@@ -1,80 +1,35 @@
-import React, { useEffect, useRef } from "react"
+import React from "react"
 import Navbar from "../Components/Navbar"
-import Swiper from 'swiper'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import HeroZoContact from "../Assets/Acceuil/zo nirina.png"
+import IllustrationContact from "./Components/Contact/IllustrationContact"
 
-const VerticalSlider = () => {
-    const swiperRef = useRef(null);
-        useEffect(() => {
-            // Ensure that Swiper is only initialized once the component is mounted
-            swiperRef.current = new Swiper('.swiper-container', {
-            // Swiper configuration options
-            direction: 'vertical',
-            slidesPerView: 1,
-            spaceBetween: 30,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            });
-        }, []);
 
-    return (
-        <div className="swiper-container">
-        <div className="swiper-wrapper">
-            <div className="xl:px-4 max-sm:px-4 max-sm:py-4 sm:max-md:px-4 sm:max-md:py-4 md:max-lg:px-4 md:max-lg:py-4 lg:max-xl:px-2 lg:max-xl:py-2 lg:max-xl:mt-0 xl:py-4 xl:mt-6 bg-gray-800 rounded-lg shadow-lg dark:bg-[#FEF5EA]">
-                <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 mx-auto text-[#FEF5EA] bg-indigo-500 rounded-md">
-                        <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
-                        </svg>
-                    </div>
+
+function HeroContact(){
+    return(
+        <div className="w-full h-[100dvh] grid grid-cols-2">
+            <section className="border border-green-400 flex items-center justify-center pt-[8rem]">
+                <img src={HeroZoContact} alt="HeroZoContact" />
+            </section> 
+            <section className="border border-green-400 flex items-center justify-center">
+                <div className="border border-red-400 w-[80%]">
+                    <section>
+                        <h1 className="text-[2.3rem] font-Kalam">Si vous avez besoin un Intégrateur et Front-end de faire votre projet, <span>en parlant ici!</span></h1>
+                    </section>
+                    <section></section>
                 </div>
-                <h3 className="max-sm:py-4 sm:max-md:py-4 md:max-lg:py-4 xl:py-4 text-2xl font-Kalam font-semibold text-[#FEF5EA] sm:text-xl dark:text-gray-700">
-                    Concerne sur moi
-                </h3>
-                <p className="max-sm:py-4 sm:max-md:py-4 md:max-lg:py-4 xl:py-4 text-gray-300 font-Kalam text-md dark:text-gray-500">
-                    Mon amour pour la construction du front-end des sites web s'accompagne d'une passion pour surmonter les obstacles. Je suis prêt à relever tout défi qui me permettra d'exploiter pleinement mes compétences et de contribuer de manière significative.
-                </p>
-            </div>
-            <div className="xl:px-4 max-sm:px-4 max-sm:py-4 sm:max-md:px-4 sm:max-md:py-4 md:max-lg:px-4 md:max-lg:py-4 lg:max-xl:px-2 lg:max-xl:py-2 lg:max-xl:mt-0 xl:py-4 xl:mt-6 bg-gray-800 rounded-lg shadow-lg dark:bg-[#FEF5EA]">
-            <div className="flex-shrink-0">
-                <div className="flex items-center justify-center w-12 h-12 mx-auto text-[#FEF5EA] bg-indigo-500 rounded-md">
-                    <svg width="20" height="20" fill="currentColor" class="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                        </path>
-                    </svg>
-                </div>
-            </div>
-            <h3 className="max-sm:py-4 sm:max-md:py-4 md:max-lg:py-4 xl:py-4 text-2xl font-Kalam font-semibold text-[#FEF5EA] sm:text-xl dark:text-gray-700">
-                Concerne sur moi
-            </h3>
-            <p className="max-sm:py-4 sm:max-md:py-4 md:max-lg:py-4 xl:py-4 text-gray-300 font-Kalam text-md dark:text-gray-500">
-                Mon amour pour la construction du front-end des sites web s'accompagne d'une passion pour surmonter les obstacles. Je suis prêt à relever tout défi qui me permettra d'exploiter pleinement mes compétences et de contribuer de manière significative.
-            </p>
+            </section>
         </div>
-        </div>
-        <div className="swiper-pagination"></div>
-        <div className="swiper-button-next"></div>
-        <div className="swiper-button-prev"></div>
-    </div>
-    );
+    )
 };
-
 
 export function Contact(){
     return(
         <>
-        <Navbar/>
-        <div>
-        <VerticalSlider/>
+        <div className="grid place-items-center w-full h-[100dvh] max-sm:h-[200dvh] sm:max-md:h-[180dvh] md:max-lg:h-[150dvh] relative acceuil-body">
+            <Navbar/>
+            <HeroContact/>
+            <IllustrationContact/>
         </div>
         </>
     )
--- my-portfolio/src/Pages/Reconnaissance.jsx
+++ my-portfolio/src/Pages/Reconnaissance.jsx
@@ -1,13 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Navbar from "../Components/Navbar"
 import {Navigation} from "swiper/modules"
+import { Link } from "react-router-dom"
 import { Swiper, SwiperSlide } from 'swiper/react'
+import Nomena from "../Assets/Acceuil/nomena.jpg"
 import 'swiper/css'
 import 'swiper/css/navigation'
 import { Icons } from "./Acceuil"
+import IllustrationBest from "./Components/Reconnaissance/IllustrationBest"
 
 function VerticalSwipe(){
+    const [activeIndex, setActiveIndex] = useState(0);
+    const handleSlideChange = (swiper) => {
+        setActiveIndex(swiper.activeIndex);
+        };
+
+    useEffect(() => {
+        const next = document.querySelector(".swiper-button-next");
+        const prev = document.querySelector(".swiper-button-prev");
+        try{
+            if(activeIndex  === 1){
+                next.style.display = "none"
+                prev.style.display = "block"
+            }else if(activeIndex === 0){
+                prev.style.display = "none"
+                next.style.display = "block"
+
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }, [activeIndex]);
+
     return(
+        <>
         <Swiper
         spaceBetween={50}
         slidesPerView={1}
@@ -18,73 +44,67 @@ function VerticalSwipe(){
             delay: 10000,
             disableOnInteraction: false,
         }}
+        onSlideChange={handleSlideChange}
         className="swiper-container-vertical"
       >
-        <SwiperSlide key={1} className="absolute top-0 left-28">
-        <div className="relative w-64 p-4 overflow-hidden bg-white shadow-lg rounded-2xl">
-            <img alt="moto" src="/images/object/1.png" className="absolute w-40 h-40 mb-4 -right-20 -bottom-8"/>
-            <div className="w-4/6">
-                <p className="mb-2 text-lg font-medium text-gray-800">
-                    Avg
+        <SwiperSlide key={1} className="absolute top-12 left-0">
+        <div className="bg-gray-800 dark:bg-[#FEF5EA] w-[24rem] shadow-lg mx-auto rounded-xl p-4">
+                <p className="text-[#FEF5EA] font-Kalam dark:text-gray-600">
+                    <span className="text-lg font-bold text-indigo-500">
+                        “
+                    </span>
+                        Il m'a réalisé un site de portfolio vraiment excitant, il a bien suivi le concept que j'ai lui demandé.  En plus, il a même respecté le deadline de la réalisation de mon site alors que c'est bien établi 👌🤩 Je suis tellement convaincu pour son capacité et son dynamisme.
+                    <span className="text-lg font-bold text-indigo-500">
+                        ”
+                    </span>
                 </p>
-                <p className="text-xs text-gray-400">
-                    Detail is not an obsession, it is the very essence of perfection.
-                </p>
-                <p className="text-xl font-medium text-indigo-500">
-                    $399
-                </p>
-            </div>
-        </div>
-        </SwiperSlide>
-        <SwiperSlide key={2}>
-            <div className="relative w-64 p-4 overflow-hidden bg-white shadow-lg rounded-2xl">
-                    <img alt="moto" src="/images/object/1.png" className="absolute w-40 h-40 mb-4 -right-20 -bottom-8"/>
-                    <div className="w-4/6">
-                        <p className="mb-2 text-lg font-medium text-gray-800">
-                            Avg
-                        </p>
-                        <p className="text-xs text-gray-400">
-                            Detail is not an obsession, it is the very essence of perfection.
-                        </p>
-                        <p className="text-xl font-medium text-indigo-500">
-                            $399
-                        </p>
+                <div className="flex items-center mt-4">
+                    <Link to="https://coruscating-arithmetic-f57467.netlify.app/" className="relative block">
+                        <img src={Nomena} alt="profil" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+                    </Link>
+                    <div className="flex flex-col justify-between ml-2">
+                        <span className="text-sm font-semibold font-Kalam text-indigo-500">
+                            RAHAJANIRINA Nomenjanahary Tantely
+                        </span>
+                        <span className="flex items-center text-xs font-Kalam text-[#FEF5EA] dark:text-gray-400">
+                            <Link to="https://coruscating-arithmetic-f57467.netlify.app/">
+                                portfolio
+                            </Link>
+                            <img src="/icons/rocket.svg" class="w-4 h-4 ml-2"/>
+                        </span>
                     </div>
                 </div>
-        </SwiperSlide>
-        <SwiperSlide key={3}>
-            <div className="relative w-64 p-4 overflow-hidden bg-white shadow-lg rounded-2xl">
-                <img alt="moto" src="/images/object/1.png" class="absolute w-40 h-40 mb-4 -right-20 -bottom-8"/>
-                <div className="w-4/6">
-                    <p className="mb-2 text-lg font-medium text-gray-800">
-                        Avg
-                    </p>
-                    <p className="text-xs text-gray-400">
-                        Detail is not an obsession, it is the very essence of perfection.
-                    </p>
-                    <p className="text-xl font-medium text-indigo-500">
-                        $399
-                    </p>
-                </div>
             </div>
         </SwiperSlide>
-        <SwiperSlide key={4}>
-            <div className="relative w-64 p-4 overflow-hidden bg-white shadow-lg rounded-2xl">
-                <img alt="moto" src="/images/object/1.png" className="absolute w-40 h-40 mb-4 -right-20 -bottom-8"/>
-                <div className="w-4/6">
-                    <p className="mb-2 text-lg font-medium text-gray-800">
-                        Avg
-                    </p>
-                    <p className="text-xs text-gray-400">
-                        Detail is not an obsession, it is the very essence of perfection.
-                    </p>
-                    <p className="text-xl font-medium text-indigo-500">
-                        $399
-                    </p>
+        <SwiperSlide key={2} className="absolute top-12 left-0">
+        <div className=" bg-gray-800 dark:bg-[#FEF5EA] w-72 shadow-lg mx-auto rounded-xl p-4">
+                <p className="text-[#FEF5EA] dark:text-gray-600">
+                    <span className="text-lg font-bold text-indigo-500">
+                        “
+                    </span>
+                    To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
+                    <span className="text-lg font-bold text-indigo-500">
+                        ”
+                    </span>
+                </p>
+                <div className="flex items-center mt-4">
+                    <Link to="/" className="relative block">
+                        <img src="/images/person/1.jpg" alt="profil" className="mx-auto object-cover rounded-full h-10 w-10 "/>
+                    </Link>
+                    <div className="flex flex-col justify-between ml-2">
+                        <span className="text-sm font-semibold text-indigo-500">
+                            Jean Miguel
+                        </span>
+                        <span className="flex items-center text-xs text-[#FEF5EA] dark:text-gray-400">
+                            User of Tail-Kit
+                            <img src="/icons/rocket.svg" class="w-4 h-4 ml-2"/>
+                        </span>
+                    </div>
                 </div>
             </div>
         </SwiperSlide>
       </Swiper>
+        </>
     )
 }
 
@@ -93,9 +113,10 @@ export function Reconnaissance() {
         <>
         <div className="w-full h-[100dvh] max-sm:h-[200dvh] sm:max-md:h-[180dvh] md:max-lg:h-[150dvh] grid items-center justify-center relative acceuil-body">
             <Navbar/>
-            <div className="border border-red-400">
+            <div>
                 <VerticalSwipe/>
             </div>
+            <IllustrationBest/>
             <Icons/>
         </div>
         </>
