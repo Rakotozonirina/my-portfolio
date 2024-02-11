43b4c577791860fe07f740d5b5e1f0bca3e53403
Zo Nirina <tomzonirina@gmail.com>
2024-02-11 22:56:53 +0100

    feat: finish of the contact feature

--- my-portfolio/src/Pages/Contact.jsx
+++ my-portfolio/src/Pages/Contact.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import Navbar from "../Components/Navbar"
 import HeroZoContact from "../Assets/Acceuil/zo nirina.png"
 import IllustrationContact from "./Components/Contact/IllustrationContact"
+import Phone from "../Assets/Acceuil/phone.svg"
+import Email from "../Assets/Acceuil/mail.svg"
+import City from "../Assets/Acceuil/city.svg"
 
 
 
@@ -12,11 +15,65 @@ function HeroContact(){
                 <img src={HeroZoContact} alt="HeroZoContact" />
             </section> 
             <section className="border border-green-400 flex items-center justify-center">
-                <div className="border border-red-400 w-[80%]">
+                <div className="grid gap-2 w-[80%]">
                     <section>
-                        <h1 className="text-[2.3rem] font-Kalam">Si vous avez besoin un Intégrateur et Front-end de faire votre projet, <span>en parlant ici!</span></h1>
+                        <h1 className="text-[2.3rem] font-semibold font-Kalam">Si vous avez besoin un Intégrateur et Front-end de faire votre projet, <span className="text-[#3563E9]">en parlant ici!</span></h1>
                     </section>
-                    <section></section>
+                    <section>
+                        <h2 className="font-Kalam text-[#757575] text-[1.8rem]">je suis disponible à vous!</h2>
+                        <section className="border border-black bg-[#D9D9D9] p-4 rounded">
+                            <ul>
+                                <li>
+                                    <p>Contact</p>
+                                </li>
+                                <li className="py-2">
+                                    <div className="flex gap-2 items-center">
+                                        <section>
+                                            <img src={Phone} alt="Phone"/>
+                                        </section>
+                                        <article>
+                                            <p>
+                                                +261329719391
+                                            </p>
+                                            <p>
+                                                Mobile
+                                            </p>
+                                        </article>
+                                    </div>
+                                </li>
+                                <li className="py-2">
+                                    <div className="flex gap-2 items-center">
+                                        <section>
+                                            <img src={Email} alt="Phone"/>
+                                        </section>
+                                        <article>
+                                            <p>
+                                                tomzonirina@gmail.com
+                                            </p>
+                                            <p>
+                                                Email
+                                            </p>
+                                        </article>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div className="flex gap-2 items-center">
+                                        <section>
+                                            <img src={City} alt="Phone"/>
+                                        </section>
+                                        <article>
+                                            <p>
+                                                Madagascar/ Mahajanga 
+                                            </p>
+                                            <p>
+                                                Pays/ Ville
+                                            </p>
+                                        </article>
+                                    </div>
+                                </li>
+                            </ul>
+                        </section>
+                    </section>
                 </div>
             </section>
         </div>
