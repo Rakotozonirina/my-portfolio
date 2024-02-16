1f075550c66e1226b9b492dba94b9ce941988e42
Zo Nirina <tomzonirina@gmail.com>
2024-02-16 07:25:31 +0100

    feat: finish of the feature

--- my-portfolio/src/Components/Lang.jsx
+++ my-portfolio/src/Components/Lang.jsx
@@ -1,25 +1,47 @@
+import { useEffect, useState } from "react"
+import { Listbox, Transition } from '@headlessui/react'
 import { useTranslation } from "react-i18next"
 
 const lngs = [
     {
-        code: "fr", native: "french"
+        id:1 ,code: "fr", native: "french"
     },
     {
-        code: "en", native: "english"
+        id:2 ,code: "en", native: "english"
     }
 ];
 
 export default function Lang(){
+    const [Language, setLanguage] = useState(lngs[0]);
     const { t, i18n } = useTranslation();
+    useEffect(
+        () => {
+            setLanguage(lngs.find((lng) => lng.code === i18n.language) || lngs[0]);
+        }, [i18n.language]
+    )
     const handleTrans = (code) => {
         i18n.changeLanguage(code);
       };
     return(
-        <div>
-            {lngs.map((lng, i) => {
-            const { code, native } = lng;
-            return <button onClick={() => handleTrans(code)}>{native}</button>;
-            })}
-        </div>
+        <Listbox value={Language} onChange={setLanguage}>
+            <Listbox.Button>{Language ? Language.native : ""}</Listbox.Button>
+            <Transition
+                enter="transition duration-100 ease-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-75 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+            >
+                <Listbox.Options>
+                {lngs.map((lng, i) => {
+                const { code, native } = lng;
+                return(
+                    <Listbox.Option key={i} onClick={() => handleTrans(code)}>{native}</Listbox.Option>
+                )
+                })}
+                </Listbox.Options>
+            </Transition>
+        </Listbox>
     )
 }
